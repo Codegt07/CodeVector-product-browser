946b427904069f4b07df7f42ce43bec216576d35
Gautam <gautam.71070@gmail.com>
2026-06-23 09:16:56 +0530

    Fix Render route issue

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getProducts } = require("../controllers/productController");
+const { getProducts } = require("../Controllers/productController");
 
 const router = express.Router();
 
