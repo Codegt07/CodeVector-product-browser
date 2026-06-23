da1e27cce8442e2dd7f348e6af6b57cd53a56abd
Gautam <gautam.71070@gmail.com>
2026-06-23 09:37:33 +0530

    Update frontend API URL for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = "https://codevector-product-browser-oycb.onrender.com";
 
 const categories = [
   "All",
